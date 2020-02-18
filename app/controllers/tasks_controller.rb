c2d159974ad74afef93cb669277f0935a3178cf7
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-02-18 15:53:16 +0000

    finished with rails-task-manager excercise. Unfinished optional and css

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,47 @@
 class TasksController < ApplicationController
+  def index
+    # Retreive all tasks
+    @tasks = Task.all
+  end
+
+  def new
+    # Create new task so you can use form_for in html
+    @task = Task.new
+  end
+
+  def show
+    # Show task with specific id
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    params[:task][:completed] = params[:task][:completed] == 1
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    params[:task][:completed] = params[:task][:completed] == 1
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # List of the tasks
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks/create', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id/update', to: 'tasks#update', as: :update
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
+  get 'tasks/:id', to: 'tasks#show', as: :show
 end
